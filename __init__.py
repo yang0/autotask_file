1064f3850bb51130b98ee2b52d687c3f1caa06f4
yang0 <yang0@126.com>
2025-01-10 19:21:15 +0800

    asdf

--- __init__.py
+++ __init__.py
@@ -3,9 +3,9 @@ from .index import *
 # test
 
 
-VERSION = "1.0.13"
+VERSION = "1.0.14"
 GIT_URL="https://github.com/yang0/autotask_file"
-NAME="文件相关操作a"
+NAME="文件相关操作"
 DESCRIPTION="文件操作插件"
 
 TAGS = ["文件处理"]
